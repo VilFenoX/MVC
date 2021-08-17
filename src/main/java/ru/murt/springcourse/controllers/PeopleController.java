326ee0d5be58e3d5b984904819124c5ddbb24a45
VilFenoX <68443337+VilFenoX@users.noreply.github.com>
2021-08-17 22:34:33 +0400

    made DAO

--- src/main/java/ru/murt/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/murt/springcourse/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ru.murt.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.murt.springcourse.dao.PersonDAO;
+import ru.murt.springcourse.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -31,4 +30,14 @@ model.addAttribute("people", personDAO.index());
         model.addAttribute("person",personDAO.show(id));
         return "people/show";
     }
+@GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person){
+   // model.addAttribute("person", new Person());
+    return "people/new";
+}
+@PostMapping()
+public String create(@ModelAttribute("person") Person person){
+personDAO.save(person);
+return "redirect:/people";
+}
 }
